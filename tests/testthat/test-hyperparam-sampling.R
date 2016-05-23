5adbdb6ff44a57f134a1167a476ca23d8e7f89b9
Ramnath Vaidyanathan <ramnath.vaidya@gmail.com>
2016-05-22 21:17:13 -0700

    use test_that and NOT testthat.

--- tests/testthat/test-hyperparam-sampling.R
+++ tests/testthat/test-hyperparam-sampling.R
@@ -1,5 +1,8 @@
 context("test-hyperparameter-sampling")
 
-testthat("quantile_sample function correctly computes inverse", {
-  expect_equal(quantile_sample("normal", seq(from = 0, to = 1, by = .25), list(mean = 0, sd = 1)), c(-Inf, qnorm(.25), 0, qnorm(.75), 1))
+test_that("quantile_sample function correctly computes inverse", {
+  expect_equal(
+    quantile_sample("normal", seq(from = 0, to = 1, by = .25), list(mean = 0, sd = 1)), 
+    c(-Inf, qnorm(.25), 0, qnorm(.75), Inf)
+  )
 })
