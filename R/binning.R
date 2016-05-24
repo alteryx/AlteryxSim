8e5494e30e341f6240db1e7ee26d339d8407b369
Dylan Blanchard <dylblanchard@gmail.com>
2016-05-24 14:51:57 -0500

    no longer need string_to_bin function

--- R/binning.R
+++ R/binning.R
@@ -1,24 +1,3 @@
-#' Converts json string to dataframe with columns 'value' and 'count'
-#'
-#' @param incoming json string with ids and values
-#' @param bin_split splitting character between bins
-#' @param id_split splitting character between id and bins
-#' @export
-#' @return dataframe of ids and values
-#' @examples
-#'    string_to_bin('{"0": 1, "1": 10, "2": 30}')
-string_to_bin <- function (incoming, bin_split=",", id_split=":") {
-  incoming <- gsub(" ", "", toString(incoming), fixed=TRUE)
-  incoming <- gsub('"', "", incoming, fixed = TRUE)
-  incoming <- substr(incoming, 2, nchar(incoming)-1)
-  bins <- unlist(strsplit(incoming,bin_split))
-  idVal_pair <- mapply(FUN=strsplit, bins, c(id_split), USE.NAMES = FALSE)
-  ids <- as.numeric(unlist(lapply(idVal_pair, '[[', 1)))
-  vals <- as.integer(unlist(lapply(idVal_pair, '[[', 2)))
-  data.frame(id = ids, count = vals)
-}
-
-
 #' Converts binned data into vector of data
 #'
 #' @param bins dataframe with columns for id and count
