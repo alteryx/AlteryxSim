bdd39c61ec6fd8948d9f76684c485c01a67506a1
Dylan Blanchard <dylblanchard@gmail.com>
2016-09-19 16:49:43 -0500

    adjust and add tests for change of bin_to_data to take count argument

--- tests/testthat/test-binning.R
+++ tests/testthat/test-binning.R
@@ -19,14 +19,28 @@ test_that("bin_to_data throws error on bins of various width", {
   expect_that(bin_to_data(binnedData), throws_error("bins must have equal width"))
 })
 
-test_that("bin_to_data keeps correct number of items in each bin", {
+test_that("bin_to_data keeps correct number of items in each bin for count = NULL", {
   lower <- 0
   upper <- 50
   by <- 10
-  actual <- c(7, 13, 5, 15, 10)
+  expected <- c(7, 13, 5, 15, 10)
   binnedData <- data.frame(id = seq(from = lower, to = upper - by, by = by), count = actual)
   vecData <- bin_to_data(binnedData)
   cuts <- cut(vecData, seq(from = lower, to = upper, by = by))
-  expected <- as.vector(table(cuts))
-  expect_that(expected, equals(actual))
+  actual <- as.vector(table(cuts))
+  expect_that(actual, equals(expected))
+})
+
+test_that("bin_to_data keeps approximately correct number of items in each bin for given count", {
+  lower <- 0
+  upper <- 50
+  by <- 10
+  count <- 76
+  expected <- c(7, 13, 5, 15, 10)
+  binnedData <- data.frame(id = seq(from = lower, to = upper - by, by = by), count = actual)
+  vecData <- bin_to_data(binnedData, count = count)
+  cuts <- cut(vecData, seq(from = lower, to = upper, by = by))
+  actual <- as.vector(table(cuts))
+  normExpected <- round(expected *count / sum(expected), 0)
+  expect_true(all(abs(normExpected - actual) <= 1))
 })
