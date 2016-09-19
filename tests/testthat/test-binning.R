b1a2f0c2fe9c1f49c389a6e4a0df4045e403e170
Dylan Blanchard <dylblanchard@gmail.com>
2016-09-19 12:36:51 -0500

    add test for bin_to_data to ensure bin content is same as expected

--- tests/testthat/test-binning.R
+++ tests/testthat/test-binning.R
@@ -18,3 +18,15 @@ test_that("bin_to_data throws error on bins of various width", {
   binnedData <- data.frame(id = c(0,1,4), count = c(1,10,30))
   expect_that(bin_to_data(binnedData), throws_error("bins must have equal width"))
 })
+
+test_that("bin_to_data keeps correct number of items in each bin", {
+  lower <- 0
+  upper <- 50
+  by <- 10
+  actual <- c(7, 13, 5, 15, 10)
+  binnedData <- data.frame(id = seq(from = lower, to = upper - by, by = by), count = actual)
+  vecData <- bin_to_data(binnedData)
+  cuts <- cut(vecData, seq(from = lower, to = upper, by = by))
+  expected <- as.vector(table(cuts))
+  expect_that(expected, equals(actual))
+})
