a4d2ac07ad78343f784bb9bb726d86d2a2acdbbd
Dylan Blanchard <dylblanchard@gmail.com>
2016-09-19 16:30:41 -0500

    edit bin_to_data function to take count argument

--- R/binning.R
+++ R/binning.R
@@ -1,10 +1,22 @@
 #' Converts binned data into vector of data
 #'
 #' @param bins dataframe with columns for id and count
+#' @param count number of samples to draw
 #' @param discrete whether the data is discrete (default is False)
 #' @return convert bins into vector of data
 #' @export
-bin_to_data <- function (bins, discrete = FALSE) {
+bin_to_data <- function (bins, count = NULL, discrete = FALSE) {
+  if(!is.null(count)) {
+    bins$count <- round(bins$count * (count/sum(bins$count)), 0)
+    total <- sum(bins$count)
+    needed <- count - total
+    idsToChange <- sample(bins$id, needed)
+    if(needed > 0) {
+      bins$count <- bins$count + bins$id %in% idsToChange
+    } else if(needed < 0) {
+      bins$count <- bins$count - bins$id %in% idsToChange
+    }
+  } 
   idVec <- bins$id
   if(!discrete) {
     idVec <- as.numeric(idVec)
